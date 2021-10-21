55bd12f4130f66e625d5dff559cae3ae4da70fb9
cerity06 <jonathan.mekdoul@gmail.com>
2021-10-21 13:42:11 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '97bd2605a4231d6ca20c555eb3246a36'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '432a420b-8533-45f7-9994-f5113b539428'
+};
